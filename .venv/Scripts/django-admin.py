e1882bb2ea381c951776fe60d1a92abf03f5faf0
Vaishnavi-Khadpe <155750129+Vaishnavi-Khadpe@users.noreply.github.com>
2024-01-05 22:31:26 +0530

    First Commit

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Sahil\Downloads\online-job-management-system-python-django\online job management system python django\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
